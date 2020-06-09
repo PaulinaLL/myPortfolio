6384d276841a565464c9406e469061b34da6fb18
PaulinaLL <paulinalll90@wp.pl>
2020-06-09 21:24:01 +0200

    Create Footer component

---
+++ src/component/Footer/index.js
@@ -0,0 +1,10 @@
+import scss from "./index.scss";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyrights 2020</p>
+    </div>
+  );
+}
