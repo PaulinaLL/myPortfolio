7bc1910eae7326b0f5064cab134ebecaa94dc6fb
PaulinaLL <paulinalll90@wp.pl>
2020-06-09 21:25:23 +0200

    Create Projects Component

---
+++ src/component/Projects/index.js
@@ -0,0 +1,14 @@
+import scss from "./index.scss";
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="projectsContainer" id="projects">
+      <div>Projekt1</div>
+      <div>Projekt2</div>
+      <div>Projekt3</div>
+      <div>Projekt4</div>
+      <div>Projekt5</div>
+    </div>
+  );
+}
