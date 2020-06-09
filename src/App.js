6bb206643459c5323e18112b27d8fabb97a4dcfa
PaulinaLL <paulinalll90@wp.pl>
2020-06-09 21:23:21 +0200

    Import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Hero from "./component/hero";
+import Hero from "./component/Hero";
+import TechStack from "./component/TechStack";
+import Footer from "./component/Footer";
+import Projects from "./component/Projects";
 import "./styles/App.scss";
 
 function App() {
   return (
     <main>
       <Hero />
+      <TechStack />
+      <Projects />
+      <Footer />
     </main>
   );
 }
